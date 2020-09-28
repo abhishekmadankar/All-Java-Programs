859842585cf72d0e944de5dfe9e3d18a13baf7d1
abhishekmadankar <48325942+abhishekmadankar@users.noreply.github.com>
2020-09-28 14:24:56 +0530

    Add files via upload

---
+++ add1.java
@@ -0,0 +1,21 @@
+class Add1{
+
+	void display(char[] temp){
+	for(char x : temp){
+		System.out.println(x);
+}
+
+		
+		
+}
+
+public static void main(String[] args){
+
+	Add1 ad = new Add1();
+	char arr[] = {'a','b','c','d'};
+	ad.display(arr);	
+	
+
+
+}
+}
